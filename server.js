acf049a0b3b1de4e6b811bd7f89b549018f466b8
hqw-112 <836861836@qq.com>
2022-05-27 10:42:03 +0800

    66

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let server = http.createServer(function(req ,res){
 if(req.url==='/api/users'){
   res.end(JSON.stringify(users))
 }else{
-  res.end('Not Found hh4455')
+  res.end('Not Found hh445566')
 }
 })
 server.listen(3000,()=>{
